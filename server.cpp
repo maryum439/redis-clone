2921932edebc8cf16e8aa71a9252f34df99e8694
Maryum439 <maryummustafa106@gmail.com>
2026-07-20 23:43:23 +0500

    Stage 4: add LPUSH, LRANGE with negative index support

--- server.cpp
+++ server.cpp
@@ -8,7 +8,16 @@
 #include <string>     
 #include <cstdlib> 
 #include <unordered_map>
-std::string handleCommand(std::vector<std::string>& args, std::unordered_map<std::string,std::string>& store){
+struct Value {
+    enum Type {
+        STRING,
+        LIST
+    };
+    Type type;
+    std::string stringValue;
+    std::vector<std::string> listValue;
+};
+std::string handleCommand(std::vector<std::string>& args, std :: unordered_map<std::string, Value> &store){
     if (args.empty()){
         return "empty args";
     }
@@ -16,7 +25,10 @@ std::string handleCommand(std::vector<std::string>& args, std::unordered_map<std
         if (args.size()!=3){
             return "-ERR wrong number of arguments for 'SET'\r\n";
         }
-        store[args[1]] = args[2];
+        Value value;
+        value.type = Value::STRING;
+        value.stringValue = args[2];
+        store[args[1]] = value;
         return "+OK\r\n";
     }
     else if (args[0] == "GET"){
@@ -24,10 +36,13 @@ std::string handleCommand(std::vector<std::string>& args, std::unordered_map<std
             return "-ERR wrong number of arguments for 'GET'\r\n";
         }
         auto it = store.find(args[1]);
-        if (it == store.end()){
+         if (it == store.end()){
             return "$-1\r\n";
         }
-        std::string x = it->second;
+        if (it->second.type != Value::STRING){
+            return "-WRONGTYPE Operation against a key holding the wrong kind of value\r\n";
+        }
+        std::string x = it->second.stringValue;
         return "$"+std::to_string(x.length())+"\r\n"+x+"\r\n";
     }
     else if (args[0]=="PING"){
@@ -61,6 +76,73 @@ std::string handleCommand(std::vector<std::string>& args, std::unordered_map<std
             return "-ERR wrong number of arguments for 'DBSIZE'\r\n";
         }
         return ":"+std::to_string(store.size()) + "\r\n";
+    }
+    else if (args[0]=="LPUSH"){
+        if (args.size() < 3){
+            return "-ERR wrong number of arguments for 'LPUSH'\r\n";
+        }
+        auto it = store.find(args[1]);
+        if (it == store.end()){
+            Value value;
+            value.type = Value::LIST;
+            for (int i = 2; i < args.size(); i++){
+                value.listValue.insert(value.listValue.begin(),args[i]);
+            }
+            store[args[1]]=value;
+            return ":" + std::to_string(value.listValue.size()) + "\r\n";
+        }
+        else if (it->second.type != Value::LIST){
+            return "-WRONGTYPE Operation against a key holding the wrong kind of value\r\n";
+        }
+        else{
+            for (int i = 2;  i < args.size(); i++){
+                it->second.listValue.insert(it->second.listValue.begin(),args[i]);
+            }
+            return ":" + std::to_string(it->second.listValue.size()) + "\r\n";
+        }
+    }
+    else if (args[0]=="LRANGE"){
+        if (args.size() < 4){
+            return "-ERR wrong number of arguments for 'LRANGE'\r\n";
+        }
+        auto it = store.find(args[1]);
+        if (it == store.end()){
+            return "*0\r\n";
+        }
+        if (it->second.type!=Value::LIST){
+            return "-WRONGTYPE Operation against a key holding the wrong kind of value\r\n";
+        }
+        int start = std:: stoi(args[2]);
+        if (start<0){
+            start = it->second.listValue.size()+start;
+        }
+        if (start <0){
+            start = 0;
+        }
+        else if (start >= it->second.listValue.size()){
+            return "*0\r\n";
+        }
+        int end = std:: stoi(args[3]);
+        if (end <0){
+            end = it->second.listValue.size()+end;
+        }
+        if (end >=it->second.listValue.size()){
+            end = it->second.listValue.size()-1;
+        }
+        if (start > end){
+            return "*0\r\n";
+        }
+        int count = 0;
+        for (int i = start; i <= end; i++){
+            count ++;
+        }
+        std:: string str = "*" + std::to_string(end - start+1) + "\r\n";
+        for (int i = start; i <= end; i++) {
+            std::string element = it->second.listValue[i];
+            str += "$" + std::to_string(element.length()) + "\r\n";
+            str += element + "\r\n";
+        }
+        return str;
     }
     else{
         return "error";
@@ -96,7 +178,7 @@ std::vector<std::string> parseRESP(char* buffer, ssize_t bytes){
     return args;
 }
 int main (){
-std::unordered_map<std::string, std::string> store;
+std::unordered_map<std::string, Value> store;
 char buffer[1024];
 int server_fd = socket(AF_INET,SOCK_STREAM,0);
 if (server_fd < 0){

6257733ad3731f970da5902fd32e95e78bd22b7b
Maryum439 <maryummustafa106@gmail.com>
2026-07-21 17:48:45 +0500

    Stage 4: add LPUSH, LRANGE with negative index support

--- server.cpp
+++ server.cpp
@@ -8,16 +8,55 @@
 #include <string>     
 #include <cstdlib> 
 #include <unordered_map>
+#include <chrono>
 struct Value {
     enum Type {
         STRING,
-        LIST
+        LIST,
+        HASH
     };
     Type type;
     std::string stringValue;
     std::vector<std::string> listValue;
+    std::unordered_map<std::string, std::string> hashValue;
 };
-std::string handleCommand(std::vector<std::string>& args, std :: unordered_map<std::string, Value> &store){
+bool isExpired(
+    const std::string& key,
+    std::unordered_map<std::string, Value>& store,
+    std::unordered_map<std::string, long long>& expiry){
+        auto expiryIt = expiry.find(key);
+        if (expiryIt == expiry.end()){
+            return false;
+        }
+        long long now = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+        if (now>= expiryIt->second){
+            store.erase(key);
+            expiry.erase(expiryIt);
+            return true;
+        }
+        return false;
+    }
+void removeExpiredKeys(
+    std::unordered_map<std::string, Value>& store,
+    std::unordered_map<std::string, long long>& expiry
+) {
+    long long now =
+        std::chrono::duration_cast<std::chrono::seconds>(
+            std::chrono::system_clock::now().time_since_epoch()
+        ).count();
+
+    for (auto it = expiry.begin(); it != expiry.end();) {
+
+        if (now >= it->second) {
+            store.erase(it->first);
+            it = expiry.erase(it);
+        }
+        else {
+            it++;
+        }
+    }
+}
+std::string handleCommand(std::vector<std::string>& args, std :: unordered_map<std::string, Value> &store,std::unordered_map<std::string, long long>& expiry){
     if (args.empty()){
         return "empty args";
     }
@@ -29,11 +68,15 @@ std::string handleCommand(std::vector<std::string>& args, std :: unordered_map<s
         value.type = Value::STRING;
         value.stringValue = args[2];
         store[args[1]] = value;
+        expiry.erase(args[1]);
         return "+OK\r\n";
     }
     else if (args[0] == "GET"){
         if (args.size()!=2){
             return "-ERR wrong number of arguments for 'GET'\r\n";
+        }
+        if (isExpired(args[1], store, expiry)){
+            return "$-1\r\n";
         }
         auto it = store.find(args[1]);
          if (it == store.end()){
@@ -57,7 +100,11 @@ std::string handleCommand(std::vector<std::string>& args, std :: unordered_map<s
         }
         int deleted = 0;
         for (int i =1; i < args.size();i++){
-            deleted += store.erase(args[i]);
+            isExpired(args[i], store, expiry);
+            if (store.erase(args[i])) {
+                expiry.erase(args[i]);
+                deleted++;
+            }
         }
         return ":"+std::to_string(deleted)+"\r\n";
     }
@@ -67,7 +114,9 @@ std::string handleCommand(std::vector<std::string>& args, std :: unordered_map<s
         }
         int existing = 0;
         for (int i = 1; i< args.size();i++){
+           if (!isExpired(args[i], store, expiry)) {
             existing += store.count(args[i]);
+        } 
         }
         return ":"+std::to_string(existing)+"\r\n";
     }
@@ -75,12 +124,14 @@ std::string handleCommand(std::vector<std::string>& args, std :: unordered_map<s
         if (args.size()!=1){
             return "-ERR wrong number of arguments for 'DBSIZE'\r\n";
         }
+        removeExpiredKeys(store, expiry);
         return ":"+std::to_string(store.size()) + "\r\n";
     }
     else if (args[0]=="LPUSH"){
         if (args.size() < 3){
             return "-ERR wrong number of arguments for 'LPUSH'\r\n";
         }
+        isExpired(args[1], store, expiry);
         auto it = store.find(args[1]);
         if (it == store.end()){
             Value value;
@@ -105,6 +156,7 @@ std::string handleCommand(std::vector<std::string>& args, std :: unordered_map<s
         if (args.size() < 4){
             return "-ERR wrong number of arguments for 'LRANGE'\r\n";
         }
+        isExpired(args[1], store, expiry);
         auto it = store.find(args[1]);
         if (it == store.end()){
             return "*0\r\n";
@@ -144,6 +196,105 @@ std::string handleCommand(std::vector<std::string>& args, std :: unordered_map<s
         }
         return str;
     }
+    else if (args[0]=="HSET"){
+        if (args.size() < 4){
+            return "-ERR wrong number of arguments for 'HSET'\r\n";
+        }
+        isExpired(args[1], store, expiry);
+        auto it = store.find(args[1]);
+        if (it == store.end()){
+            Value value;
+            value.type = Value::HASH;
+            value.hashValue[args[2]] = args[3];
+            store[args[1]] = value;
+            return ":1\r\n";
+        }
+        if (it->second.type!=Value::HASH){
+            return "-WRONGTYPE Operation against a key holding the wrong kind of value\r\n";
+        }
+        else if (it!=store.end()&&it->second.type==Value::HASH){
+            bool newField =it->second.hashValue.find(args[2])== it->second.hashValue.end();
+            it->second.hashValue[args[2]] = args[3];
+            return newField ? ":1\r\n" : ":0\r\n";
+        }
+    }
+    else if(args[0]=="HGET"){
+        if (args.size() < 3){
+            return "-ERR wrong number of arguments for 'HGET'\r\n";
+        }
+        isExpired(args[1], store, expiry);
+        auto it = store.find(args[1]);
+        if (it  == store.end()){
+            return "$-1\r\n";
+        }
+        if (it->second.type!=Value::HASH){
+            return "-WRONGTYPE Operation against a key holding the wrong kind of value\r\n";
+        }
+        auto field = it->second.hashValue.find(args[2]);
+        if (field  == it->second.hashValue.end()){
+            return "$-1\r\n";
+        }
+        std::string x = field->second;
+        return "$" + std::to_string(x.length()) + "\r\n"+ x + "\r\n";
+    }
+    else if (args[0]=="RPOP"){
+       if (args.size() < 2){
+            return "-ERR wrong number of arguments for 'RPOP'\r\n";
+        } 
+        isExpired(args[1], store, expiry);
+        auto it = store.find(args[1]);
+        if (it == store.end()){
+            return "$-1\r\n";
+        }
+        if (it->second.type != Value::LIST){
+            return "-WRONGTYPE Operation against a key holding the wrong kind of value\r\n";
+        }
+        if (it->second.listValue.size()==0){
+            return "$-1\r\n";
+        }
+        std :: string lElement = it->second.listValue[it->second.listValue.size()-1];
+        it->second.listValue.pop_back();
+        if (it->second.listValue.empty()){
+            store.erase(it);
+            expiry.erase(args[1]);
+        }
+        return "$" + std::to_string(lElement.length()) + "\r\n"+ lElement + "\r\n";
+    }
+    else if (args[0]=="EXPIRE"){
+        if (args.size() != 3){
+            return "-ERR wrong number of arguments for 'EXPIRE'\r\n";
+        }
+        if (isExpired(args[1], store, expiry)) {
+            return ":0\r\n";
+        }
+        auto it = store.find(args[1]);
+        if (it == store.end()){
+            return ":0\r\n";
+        }
+        long long now =std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+        long long expiryTime = now + std::stoll(args[2]);
+        expiry[args[1]] = expiryTime;
+        return ":1\r\n";
+    }
+    else if (args[0]=="TTL"){
+        if (args.size() != 2){
+            return "-ERR wrong number of arguments for 'TTL'\r\n";
+        }
+        if (isExpired(args[1], store, expiry)){
+        return ":-2\r\n";
+    }
+    auto it = store.find(args[1]);
+    if (it == store.end()){
+        return ":-2\r\n";
+    }
+    auto expiration = expiry.find(args[1]);
+    if (expiration == expiry.end()){
+        return ":-1\r\n";
+    }
+    long long now =std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    long long remaining = expiration->second - now;
+    return ":" + std::to_string(remaining) + "\r\n";
+    }
     else{
         return "error";
     }
@@ -178,6 +329,7 @@ std::vector<std::string> parseRESP(char* buffer, ssize_t bytes){
     return args;
 }
 int main (){
+std::unordered_map<std::string, long long> expiry;
 std::unordered_map<std::string, Value> store;
 char buffer[1024];
 int server_fd = socket(AF_INET,SOCK_STREAM,0);
@@ -218,7 +370,7 @@ std::vector<std::string> args = parseRESP(buffer, bytes);
 for (size_t i = 0; i < args.size(); i++) {
     printf("arg[%zu] = %s\n", i, args[i].c_str());
 }
-std::string reply = handleCommand(args, store);
+std::string reply = handleCommand(args, store,expiry);
 if (write(client_fd, reply.c_str(), reply.size())<0){
     perror("writing failed");
     break;
